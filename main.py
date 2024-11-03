ca2b5d6b125e0d1b3eaffb6f1121451c64595d31
Aakash <50060952+para8ox-deb@users.noreply.github.com>
2024-11-03 23:06:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://saroperation.netlify.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
